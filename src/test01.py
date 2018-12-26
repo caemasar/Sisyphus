3255ae28dff78e419f39e15c5c66fdf2b38105ed
caemasar <caemasar@proton.com>
2018-12-26 16:46:31 +0800

    测试了一个去标点符号的正则应用

--- src/test01.py
+++ src/test01.py
@@ -1,3 +1,10 @@
 #coding=utf=8
 
 print("End")
+
+import re
+temp = "想做/ 兼_职/学生_/ 的 、加,我Q：  1 5.  8 0. ！！？？  8 6 。0.  2。 3     有,惊,喜,哦"
+# python3 默认utf-8，用decode会报 'str' object has no attribute 'decode'
+# temp = temp.decode("utf8")
+string = re.sub("[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）]+", "",temp)
+print(string)
